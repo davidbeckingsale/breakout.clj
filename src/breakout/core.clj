1c4093e6c48ed823701c64b9a0dabd408e7c1345
David Beckingsale <davidbeckingsale@gmail.com>
2013-03-10 15:04:48 +0000

    Added a simple player

--- src/breakout/core.clj
+++ src/breakout/core.clj
@@ -2,11 +2,15 @@
     (:import (org.lwjgl LWJGLException)
      (org.lwjgl.opengl Display DisplayMode GL11)))
 
-(def world-size [4 2])
+(def world-size [8 5])
 (def screen-size [800 600])
 
-(defrecord World [bricks])
+(defrecord World [bricks player running])
 (defrecord Brick [colour destroyed])
+(defrecord Player [position velocity])
+
+(defn init-player []
+     (->Player [350 580] [0 0]))
 
 (def bricks
      {:red (->Brick [1.0 0.0 0.0] nil)
@@ -30,23 +34,34 @@
 
     (GL11/glBegin GL11/GL_QUADS)
     (GL11/glVertex2f x y)
-    (GL11/glVertex2f (+ x 200) y)
-    (GL11/glVertex2f (+ x 200) (+ y 150))
-    (GL11/glVertex2f x (+ y 150))
+    (GL11/glVertex2f (+ x 100) y)
+    (GL11/glVertex2f (+ x 100) (+ y 60))
+    (GL11/glVertex2f x (+ y 60))
     (GL11/glEnd)))
 
 (defn random-world []
-  (->World (random-bricks)))
+   (->World (random-bricks) (init-player) true))
 
 (defn draw-world [world]
   (let [[x y] world-size
         [gx gy] screen-size
-        bx 200
-        by 150]
+        bx 100
+        by 60]
     (doseq [row (range 0 y)]
       (doseq [col (range 0 x)]
         (draw-brick (nth (nth (:bricks world) row) col)
                     [(* col bx) (* row by)])))))
+
+(defn draw-player [player]
+  (let [[x y] (:position player)]
+    (GL11/glColor3f 1.0 0.0 0.0)
+
+    (GL11/glBegin GL11/GL_QUADS)
+    (GL11/glVertex2f x y)
+    (GL11/glVertex2f (+ x 100) y)
+    (GL11/glVertex2f (+ x 100) (+ y 20))
+    (GL11/glVertex2f x (+ y 20))
+    (GL11/glEnd)))
 
 (defn init []
   (let [[x y] screen-size]
@@ -61,11 +76,13 @@
   (random-world))
 
 (defn render-loop [world]
-  (while (not (Display/isCloseRequested))
-    (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
-    (draw-world world)
-    (Display/update)
-    (Display/sync 60)))
+  (while (and (:running world)
+              (not (Display/isCloseRequested)))
+         (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
+         (draw-world world)
+         (draw-player (:player world))
+         (Display/update)
+         (Display/sync 60)))
 
 (defn finalise []
   (Display/destroy))
